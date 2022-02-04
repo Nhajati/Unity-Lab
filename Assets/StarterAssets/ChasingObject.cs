ad5542630f141c68cf0d4dbf6bd4dec4f3e25b55
Nasim Hajati <nasim.hajati987@gmail.com>
2022-02-04 11:44:12 -0800

    Substituted the sphere with a robot. Robot is following the midpoint correctly. Need to modify the scoring and fix the problem of 20 points under the tunnel!

--- Assets/StarterAssets/ChasingObject.cs
+++ Assets/StarterAssets/ChasingObject.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class ChasingObject : MonoBehaviour
 {
     Vector3 direction;
+    Vector3 characterSize;
+    Vector3 characterMidPoint;
     CharacterController myCharacter;
-    // Start is called before the first frame update
+
     void Start()
     {
+        // Find the character
         var character = FindObjectsOfType<CharacterController>();
         myCharacter = character[0];
-        var pos = character[0].transform.position;
-        direction = pos - transform.position;
-        // transform.forward = direction;
+
+        // Get the height of the character, so that we could command the robot to follow its middle point
+        characterSize = myCharacter.GetComponent<Collider>().bounds.size; 
+        characterMidPoint = new Vector3(myCharacter.transform.position.x, characterSize.y / 2, myCharacter.transform.position.z);
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        direction = myCharacter.transform.position - transform.position;
+        // Update the midpoint position of the character
+        characterMidPoint.x = myCharacter.transform.position.x;
+        characterMidPoint.y = characterSize.y / 2;
+        characterMidPoint.z = myCharacter.transform.position.z;
+        direction = characterMidPoint - transform.position;
+        
+        // Make the enemy object face the character
+        transform.forward = direction;
+        // Debug.DrawRay(transform.position, transform.forward, Color.red); // Need to turn on Gizmos to make it show!
+
+        // Make the enemy object follow the character 
         transform.Translate(direction * 0.005f);
     }
 }
