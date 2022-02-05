13411814325086f543b5a64d59e2c45aa31c9a4b
Nasim Hajati <nasim.hajati987@gmail.com>
2022-02-05 14:54:35 -0800

    My sunflowers are rotating now :) There is an error with the ChasingObject file, look rotation viewing vector is zero, I need to fix this.

--- Assets/StarterAssets/RandomRotation.cs
+++ Assets/StarterAssets/RandomRotation.cs
@@ -5,41 +5,27 @@ using UnityEngine;
 
 public class RandomRotation : MonoBehaviour
 {
-    float theta;
-    float previousTheta;
-    float incrementsOfTheta;
-    static float smooth = 0.5f;
 
-    void Start() 
-    {
-        transform.rotation = new Quaternion(20, 20, 20, -1);
-        theta = Random.Range(-360.0f, 360.0f);
-        previousTheta = 0;
+    static float smoothe = 1.0f;
+    Quaternion target;
+
+    void Start(){
+        target = Random.rotation;
     }
+    void Update() {
 
-
-    void Update()
-    {
-        Quaternion target = Quaternion.Euler(0, theta, 0);
-
-        if ((transform.eulerAngles - target.eulerAngles).magnitude < 0.001f) 
-        {
-            previousTheta = theta;
-            theta = Random.Range(-360.0f, 360.0f);
+        if ((transform.GetChild(0).eulerAngles - Quaternion.Euler(0, target.eulerAngles.y, 0).eulerAngles).magnitude < 0.1f) {
+            target = Random.rotation;
         }
-
-        float timeNow = Time.deltaTime;
 
         for(int i = 0; i < transform.childCount; i++)
         {
             var child = transform.GetChild(i);
-            Debug.Log(transform.rotation);
-            // THE PROBLEM IS THAT TRANSFORM.ROTATION IS ZERO IN THE FIRST MOMENT.
-            child.transform.rotation = Quaternion.Slerp(transform.rotation, target, timeNow * smooth);
+            child.transform.rotation = Quaternion.Slerp(child.transform.rotation, Quaternion.Euler(0, target.eulerAngles.y, 0), Time.deltaTime * smoothe);
         } 
+        
 
     }
-
 
 }
 
