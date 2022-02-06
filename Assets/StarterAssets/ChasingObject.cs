6df1ee4215763a75fc59d77f67048b7d3f4308b0
Nasim Hajati <nasim.hajati987@gmail.com>
2022-02-05 19:31:51 -0800

    Collision detection for the robot and the character done. Need to put the health hearts into play now.

--- Assets/StarterAssets/ChasingObject.cs
+++ Assets/StarterAssets/ChasingObject.cs
@@ -6,18 +6,17 @@ namespace IGS520b.starter.SampleGame
 {    
 public class ChasingObject : MonoBehaviour
 {
-
-    // GameManager gm;
-    // NEED TO SAY CHASE AFTER START
     Vector3 direction;
     Vector3 characterSize;
     Vector3 characterMidPoint;
     CharacterController myCharacter;
-    bool isChasing;
-    // GameManager gameManager;
+    bool robotCrashed;
+    float minDistance;
+    float nowDistance;
 
     void Start()
     {
+        robotCrashed = false;
         
         // Find the character
         var character = FindObjectsOfType<CharacterController>();
@@ -32,27 +31,57 @@ public class ChasingObject : MonoBehaviour
     void Update()
     {
         GameManager[] gameManager = FindObjectsOfType<GameManager>();
+        // Debug.Log(HasCrashedIntoCharacter());
 
         if(gameManager[0].GetStartChasing()){
-        // Update the midpoint position of the character
-        characterMidPoint.x = myCharacter.transform.position.x;
-        characterMidPoint.y = characterSize.y / 2;
-        characterMidPoint.z = myCharacter.transform.position.z;
-        direction = characterMidPoint - transform.position;
-        
-        // Make the enemy object face the character
-        transform.forward = direction;
-        Debug.DrawRay(transform.position, transform.forward, Color.red); // Need to turn on Gizmos to make it show!
 
-        // Make the enemy object follow the character 
-        transform.Translate(direction * 0.005f);
+            if(HasCrashedIntoCharacter()){
+                CrashedIntoCharacter();
+            } else {
+            // Update the midpoint position of the character
+            characterMidPoint.x = myCharacter.transform.position.x;
+            characterMidPoint.y = characterSize.y / 2;
+            characterMidPoint.z = myCharacter.transform.position.z;
+            direction = characterMidPoint - transform.position;
+        
+            // Make the enemy object face the character
+            transform.forward = direction;
+            Debug.DrawRay(transform.position, transform.forward, Color.red); // Need to turn on Gizmos to make it show!
+
+            // Make the enemy object follow the character 
+            transform.Translate(direction * 0.005f);
+
+            }
         }
        
     }
 
-    void CrashedIntoCharacter() 
+    // To check if the robot crashed into the character
+    bool HasCrashedIntoCharacter() 
+    {   
+        var theCharacter = FindObjectsOfType<CharacterController>();
+        // MAKE SURE THEY HAVE ONLY ONE COLLIDER!
+        Collider characterCollider = theCharacter[0].GetComponent<Collider>(); 
+        SphereCollider robotCollider = GetComponent<SphereCollider>();
+
+        nowDistance = Mathf.Sqrt(Mathf.Pow(transform.position.x - theCharacter[0].transform.position.x, 2) + Mathf.Pow(transform.position.z - theCharacter[0].transform.position.z, 2));
+        // minDistance = Mathf.Abs(robotCollider.radius - Mathf.Sqrt(Mathf.Pow(characterCollider.bounds.size.x, 2) + Mathf.Pow(characterCollider.bounds.size.z, 2)));
+        minDistance = 0.9f;
+        Debug.Log(nowDistance);
+        Debug.Log(minDistance);
+        Debug.Log(robotCrashed);
+        
+        if(nowDistance <= minDistance){
+            robotCrashed = true;
+        } else {
+            robotCrashed = false;
+        }
+        return robotCrashed;
+    }
+
+    // What to do when the robot crashes into the character
+    void CrashedIntoCharacter()
     {
-        // if(crashed)
         // health -= 1;
     }
 }
